30d47092c893003d71887217d3754741a73e64f3
QuangNHN <quangnhn@glotechvn.com>
2023-04-06 14:58:56 +0700

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React, { useState } from 'react';
-import { HiOutlineSearch, HiOutlineShoppingBag } from 'react-icons/hi'
+import { HiMagnifyingGlass, HiOutlineShoppingCart, HiOutlineUser, HiOutlineTruck } from 'react-icons/hi2'
 
 const Header = () => {
   const [isBagItem, setIsBagItem] = useState(true);
 
   return (
-    <header className="sticky top-0 z-30 w-full flex items-center justify-center p-4 bg-slate-200">
-        <div className="flex items-center justify-center w-full lg:w-[1024px]">
+    <header className="element-center sticky top-0 z-30 w-full p-4 bg-emerald-100">
+        <div className="element-center w-full lg:w-[1024px]">
             {/* Logo */}
-            <div className="flex items-center justify-center mr-3">
+            <div className="element-center mr-3">
                 <Link href="/">
                     <div className="relative h-10 w-10 cursor-pointer opacity-75 transition hover:opacity-100">
                         <Image className="object-contain" src="https://res.cloudinary.com/nguyenhnhatquang/image/upload/v1626886904/logo_nwimwg.png" alt="" fill/>
@@ -18,22 +18,36 @@ const Header = () => {
                 </Link>
             </div>
             {/* Search */}
-            <div className="flex items-center justify-start w-full">
-                <div className="relative w-full flex items-center justify-center h-10 ">
+            <div className="element-start w-full ">
+                <div className="element-center relative w-full h-10 max-w-md">
                     <input className="h-full w-[calc(100%_-_0.5rem)] outline-none pl-6 pr-10 rounded-full" type="search" placeholder="Bạn cần tìm gì?"/>
-                    <HiOutlineSearch className="absolute top-0 right-5 h-full cursor-pointer"/>
+                    <HiMagnifyingGlass className="absolute top-0 right-5 h-full cursor-pointer"/>
                 </div>
             </div>
             {/* Right - shopping card*/}
-            <div className="flex items-center justify-start ml-3">
-                <div className="relative bg-slate-50 h-10 w-10 flex items-center justify-center rounded-xl cursor-pointer hover:bg-white">
-                    <HiOutlineShoppingBag className="absolute inset-y-0 h-full text-2xl"/>
+            <div className="element-end ml-3">
+                <div className="element-center relative bg-slate-50 h-10 w-10 rounded-xl cursor-pointer hover:bg-emerald-50 mr-[10px] min-[580px]:w-[100px]">
+                    <HiOutlineTruck className="h-full text-[18px]"/>
+                    <span className="hidden text-[14px] subpixel-antialiased pl-1 min-[580px]:block">Đơn Hàng</span>
+                </div>
+
+                <div className="element-center relative bg-slate-50 h-10 w-10 rounded-xl cursor-pointer hover:bg-emerald-50 min-[580px]:w-[100px] sm:mr-[10px]">
+                    <HiOutlineShoppingCart className="h-full text-[18px]"/>
                     {
-                        isBagItem && 
-                        <div className="absolute bg-green-600 h-4 w-4 rounded-full top-7 left-7 text-white text-xs flex text-center justify-center">
-                            10
-                        </div>
+                        isBagItem && (
+                            <>
+                                <div className="flex text-center justify-center absolute bg-green-600 h-4 w-4 rounded-full top-7 left-[30px] text-white text-[11px] min-[580px]:left-[89px]">
+                                    10
+                                </div>
+                            </>
+                        )
                     }
+                    <span className="hidden text-[14px] subpixel-antialiased pl-1 min-[580px]:block">Giỏ Hàng</span>
+                </div>
+
+                <div className="hidden h-10 w-[100px] bg-slate-50 rounded-xl cursor-pointer sm:element-center hover:bg-emerald-50">
+                    <HiOutlineUser className="h-full text-[18px]"/>
+                    <span className="hidden text-[14px] subpixel-antialiased pl-1 min-[580px]:block">Đăng Nhập</span>
                 </div>
             </div>
         </div>
