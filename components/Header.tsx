d1537248e20591882ee22fab591fb522023fdb2c
ytuzu <nguyenhnhatquang@gmail.com>
2023-04-07 23:20:32 +0700

    header done

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,10 +7,10 @@ const Header = () => {
   const [isBagItem, setIsBagItem] = useState(true);
 
   return (
-    <header className="element-center sticky top-0 z-30 w-full p-4 bg-emerald-100">
-        <div className="element-center w-full lg:w-[1024px]">
+    <header className="flex items-center sticky top-0 z-30 w-full p-4 bg-emerald-100">
+        <div className="flex items-center justify-center w-full lg:w-[1024px]">
             {/* Logo */}
-            <div className="element-center mr-3">
+            <div className="flex items-center justify-center mr-3">
                 <Link href="/">
                     <div className="relative h-10 w-10 cursor-pointer opacity-75 transition hover:opacity-100">
                         <Image className="object-contain" src="https://res.cloudinary.com/nguyenhnhatquang/image/upload/v1626886904/logo_nwimwg.png" alt="" fill/>
@@ -25,42 +25,42 @@ const Header = () => {
                 </div>
             </div>
             {/* Right - shopping card*/}
-            <div className="element-end ml-3">
-                <div className="element-center header__button mr-[10px] min-[580px]:header__button--grid min-[580px]:w-[100px]">
-                    <div className="flex items-center justify-end">
-                        <HiOutlineTruck className="h-full text-[18px]"/>
+            <div className="flex items-center justify-end ml-3">
+                <div className="header__button mr-[10px] min-[580px]:header__button--grid">
+                    <div className="flex items-center justify-center w-auto min-[580px]:justify-end">
+                        <HiOutlineTruck className="text-[18px]"/>
                     </div>
-                    <div className="flex items-center justify-start">
-                        <span className="hidden text-[13px] subpixel-antialiased pl-1 min-[580px]:block">Đơn Hàng</span>
+                    <div className="hidden text-[13px] pl-1 items-center justify-start min-[580px]:flex">
+                        <span>Đơn Hàng</span>
                     </div>
                 </div>
 
-                <div className="header__button relative w-10 min-[580px]:w-[100px] sm:mr-[10px]">
-                    <div className="flex items-center justify-end">
-                        <HiOutlineShoppingCart className="h-full text-[18px]"/>
+                <div className="header__button relative min-[580px]:header__button--grid sm:mr-[10px]">
+                    <div className="flex items-center justify-center w-auto min-[580px]:justify-end">
+                        <HiOutlineShoppingCart className="text-[18px]"/>
                     </div>
                     
                     {
                         isBagItem && (
                             <>
                                 <div className="flex text-center justify-center absolute bg-green-600 h-4 w-4 rounded-full top-7 left-[30px] text-white text-[11px] min-[580px]:left-[89px]">
-                                    10
+                                    <span>10</span>
                                 </div>
                             </>
                         )
                     }
 
-                    <div className="flex items-center justify-start">
-                        <span className="hidden text-[13px] subpixel-antialiased pl-1 min-[580px]:block">Giỏ Hàng</span>
+                    <div className="hidden text-[13px] pl-1 items-center justify-start min-[580px]:flex">
+                        <span>Giỏ Hàng</span>
                     </div>
                 </div>
 
-                <div className="hidden sm:header__button w-[100px]">
-                    <div className="flex items-center justify-end">
-                        <HiOutlineUser className="h-full text-[18px]"/>
+                <div className="hidden sm:header__button sm:header__button--grid sm:mr-[10px]">
+                    <div className="flex items-center justify-center w-auto min-[580px]:justify-end">
+                        <HiOutlineUser className="text-[18px]"/>
                     </div>
-                    <div className="flex items-center justify-start">
-                        <span className="hidden text-[13px] max-w-[70px] subpixel-antialiased pl-1 whitespace-nowrap overflow-hidden min-[580px]:block">Đăng Nhập</span>
+                    <div className="hidden text-[13px] pl-1 items-center justify-start min-[580px]:flex">
+                        <span>Đăng Nhập</span>
                     </div>
                 </div>
             </div>
